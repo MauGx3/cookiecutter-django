4676345edd065df1240fca648d6179224dbdcf2d
Maurício Gioachini <maugx3@gmail.com>
2025-09-07 14:29:27 -0300

    Revert "Fix ResourceWarning for unclosed files in test_cookiecutter_generation.py"
    This reverts commit decfd22cb7cbf31427adcae05e3e90fab1b65083.

--- tests/test_cookiecutter_generation.py
+++ tests/test_cookiecutter_generation.py
@@ -168,10 +168,9 @@ def check_paths(paths: Iterable[Path]):
         if is_binary(str(path)):
             continue
 
-        with path.open() as file:
-            for line in file:
-                match = RE_OBJ.search(line)
-                assert match is None, f"cookiecutter variable not replaced in {path}"
+        for line in path.open():
+            match = RE_OBJ.search(line)
+            assert match is None, f"cookiecutter variable not replaced in {path}"
 
 
 @pytest.mark.parametrize("context_override", SUPPORTED_COMBINATIONS, ids=_fixture_id)
